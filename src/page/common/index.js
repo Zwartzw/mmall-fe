524413c8fd327c5a23f45f173e8e98d46f2ae4bc
WillGiab <2410250284@qq.com>
2017-06-13 15:06:27 +0800

    项目初始化

---
+++ src/page/common/index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 24102 on 2017/6/13.
+ */
+
+console.log("global");
---
+++ src/page/index/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 24102 on 2017/6/13.
+ */
+
+console.log("hello index");
+$('body').html('12345');
+require('./index.css');
