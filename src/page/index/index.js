fccd5217f8836138af1cc711cf04f681d1301048
WillGiab <2410250284@qq.com>
2017-06-14 14:20:29 +0800

    侧面导航栏开发
    如何使用模板

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -6,5 +6,10 @@
 // require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/index.js');
-// require('page/common/header/index.js');
-var _mm = require('util/mm.js');
+require('page/common/header/index.js');
+var navSide = require('page/common/nav-side/index.js');
+var _mm     = require('util/mm.js');
+
+navSide.init({
+    name:'order-list'
+})
