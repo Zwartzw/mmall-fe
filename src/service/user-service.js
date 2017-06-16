1dd48211314669c0eb94d3b4857b353bdc56b931
WillGiab <2410250284@qq.com>
2017-06-16 13:46:56 +0800

    个人中心页面及编辑页面开发

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -51,6 +51,25 @@ var _user = {
             error   : reject
         });
     },
+    // 获取用户信息
+    getUserInfo:function(resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/get_information.do'),
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
+    // 更新用户信息
+    updateUserInfo:function(userInfo, resolve, reject){
+        _mm.request({
+            url     : _mm.getServerUrl('/user/update_information.do'),
+            data    : userInfo,
+            method  : 'POST',
+            success : resolve,
+            error   : reject
+        });
+    },
     // 登出
     logout:function(resolve, reject){
         _mm.request({
--- webpack.config.js
+++ webpack.config.js
@@ -22,16 +22,18 @@ var getHtmlConfig = function (name,title) {
 
 config = {
     entry: {
-        'common'            :['./src/page/common/index.js'],
-        'index'             :['./src/page/index/index.js'],
-        'user-login'        :['./src/page/user-login/index.js'],
-        'user-register'     :['./src/page/user-register/index.js'],
-        'result'            :['./src/page/result/index.js']
+        'common'                :['./src/page/common/index.js'],
+        'index'                 :['./src/page/index/index.js'],
+        'user-login'            :['./src/page/user-login/index.js'],
+        'user-register'         :['./src/page/user-register/index.js'],
+        'user-center'           :['./src/page/user-center/index.js'],
+        'user-center-update'    :['./src/page/user-center-update/index.js'],
+        'result'                :['./src/page/result/index.js']
     },
     output:{
         path:'./dist',
         publicPath : '/dist',
-        filename:'js/[name].js',
+        filename:'js/[name].js'
     },
     externals:{
         'jquery':'window.jQuery'
@@ -63,8 +65,10 @@ config = {
         //html模板处理
         new HtmlWebpackPlugin(getHtmlConfig('user-register','用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login','用户登陆')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center','个人中心')),
+        new HtmlWebpackPlugin(getHtmlConfig('user-center-update','修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
-        new HtmlWebpackPlugin(getHtmlConfig('result','操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('result','操作结果'))
     ]
 
 };
