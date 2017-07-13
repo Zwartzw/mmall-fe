2268dc8f384020ccb36a4ca45f346460ba2eded3
WillGiab <2410250284@qq.com>
2017-07-13 12:21:17 +0800

    订单确认，模态新地址

--- src/page/order-confirm/index.js
+++ src/page/order-confirm/index.js
@@ -6,22 +6,70 @@
 
 require('./index.css');
 require('page/common/header/index.js');
-var _mm             = require('util/mm.js');
-var _order          = require('service/order-service.js');
-var templateIndex   = require('./index.string');
+var _mm                 = require('util/mm.js');
+var _order              = require('service/order-service.js');
+var _address            = require('service/address-service.js');
+var templateAddress     = require('./address-list.string');
+var templateProduct     = require('./product-list.string');
 
 var page = {
-    data: {},
+    data: {
+        selectedAddress : null
+    },
     init: function () {
         this.onLoad();
         this.bindEvent();
     },
     onLoad: function () {
-        this.loadCart();
+        this.loadAddressList();
+        this.loadProductList();
     },
     bindEvent: function () {
         var _this = this;
-    }
+        // 地址的选择
+        $(document).on('click', '.address-item', function(){
+            $(this).addClass('active')
+                .siblings('.address-item').removeClass('active');
+            _this.data.selectedAddressId = $(this).data('id');
+        });
+        // 订单提交
+        $(document).on('click', '.order-submit', function(){
+            var shippingId = _this.data.selectedAddressId;
+            if(shippingId){
+                _order.createOrder({
+                    shippingId : shippingId
+                },function (res) {
+                    window.location.href = './payment.html?orderNumber=' + res.orderNo;
+                },function (errMsg) {
+                    _mm.errorTips(errMsg)
+                })
+            }else{
+                _mm.errorTips('请选择地址后提交');
+            }
+        });
+    },
+    // 加载地址列表信息
+    loadAddressList: function () {
+        var _this = this;
+        //获取地址列表
+        _address.getAddressList(function (res) {
+                var addressListHtml = _mm.renderHtml(templateAddress, res);
+                $('.address-con').html(addressListHtml);
+            },function (errMsg) {
+                $('.address-con').html('<p class="err-tip">地址加载失败，请刷新</p>');
+            });
+    },
+    // 加载地址列表信息
+    loadProductList: function () {
+        var _this = this;
+        //获取地址列表
+        _order.getProductList(function (res) {
+            var productListHtml = _mm.renderHtml(templateProduct, res);
+            $('.product-con').html(productListHtml);
+        },function (errMsg) {
+            $('.product-con').html('<p class="err-tip">商品清单加载失败，请刷新</p>');
+        });
+    },
 };
 $(function () {
     page.init();
