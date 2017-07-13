80e3ad15f20a8629612be90f4b765c695edb56a9
WillGiab <2410250284@qq.com>
2017-07-13 20:51:12 +0800

    订单详情页，完成开发

--- src/page/user-center/index.js
+++ src/page/user-center/index.js
@@ -10,23 +10,23 @@ require('page/common/header/index.js');
 var navSide         = require('page/common/nav-side/index.js');
 var _mm             = require('util/mm.js');
 var _user           = require('service/user-service.js');
-var templateIndex   = require('./index.string')
+var templateIndex   = require('./index.string');
 
 // page 逻辑部分
 var page = {
-    init:function () {
+    init         :function () {
         this.onLoad();
     },
-    onLoad:function () {
+    onLoad       :function () {
         //初始化左侧菜单
         navSide.init({
             name:'user-center'
         });
         //加载用户信息
-        this.loadUserInfo();
+        this.loadOrderList();
     },
     //加载用户信息
-    loadUserInfo : function () {
+    loadOrderList: function () {
         var userHtml = '';
         _user.getUserInfo(function (res) {
             userHtml = _mm.renderHtml(templateIndex, res);
