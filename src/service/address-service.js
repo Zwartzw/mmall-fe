a9a3dc680ee9c2c1d32fb7d56f34d58aa9cd1101
WillGiab <2410250284@qq.com>
2017-07-13 17:45:48 +0800

    订单确认页逻辑开发完成

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -5,57 +5,57 @@
 'use strict';
 var _mm = require('util/mm.js');
 
-var _address = {
+var _address   = {
     // 获取地址列表
-    getAddressList : function(resolve, reject){
+    getAddressList: function (resolve, reject) {
         _mm.request({
-            url     : _mm.getServerUrl('/shipping/list.do'),
-            data    : {
-                pageSize : 50
+            url    : _mm.getServerUrl('/shipping/list.do'),
+            data   : {
+                pageSize: 50
             },
-            success : resolve,
-            error   : reject
+            success: resolve,
+            error  : reject
         });
     },
     // 新建收件人
-    save : function(addressInfo, resolve, reject){
+    save          : function (addressInfo, resolve, reject) {
         _mm.request({
-            url     : _mm.getServerUrl('/shipping/add.do'),
-            data    : addressInfo,
-            success : resolve,
-            error   : reject
+            url    : _mm.getServerUrl('/shipping/add.do'),
+            data   : addressInfo,
+            success: resolve,
+            error  : reject
         });
     },
     // 更新收件人
-    update : function(addressInfo, resolve, reject){
+    update        : function (addressInfo, resolve, reject) {
         _mm.request({
-            url     : _mm.getServerUrl('/shipping/update.do'),
-            data    : addressInfo,
-            success : resolve,
-            error   : reject
+            url    : _mm.getServerUrl('/shipping/update.do'),
+            data   : addressInfo,
+            success: resolve,
+            error  : reject
         });
     },
     // 删除收件人
-    deleteAddress : function(shippingId, resolve, reject){
+    deleteAddress : function (shippingId, resolve, reject) {
         _mm.request({
-            url     : _mm.getServerUrl('/shipping/del.do'),
-            data    : {
-                shippingId : shippingId
+            url    : _mm.getServerUrl('/shipping/del.do'),
+            data   : {
+                shippingId: shippingId
             },
-            success : resolve,
-            error   : reject
+            success: resolve,
+            error  : reject
         });
     },
     // 获取单条收件人信息
-    getAddress : function(shippingId, resolve, reject){
+    getAddress    : function (shippingId, resolve, reject) {
         _mm.request({
-            url     : _mm.getServerUrl('/shipping/select.do'),
-            data    : {
-                shippingId : shippingId
+            url    : _mm.getServerUrl('/shipping/select.do'),
+            data   : {
+                shippingId: shippingId
             },
-            success : resolve,
-            error   : reject
+            success: resolve,
+            error  : reject
         });
     },
-}
+};
 module.exports = _address;
